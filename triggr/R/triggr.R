829a375a99d1d5412a4500d1e15edf83ad3b1ea2
Miron B. Kursa <M.Kursa@icm.edu.pl>
2011-03-31 17:25:47 +0200

    Stub of R wrapper.

---
+++ triggr/R/triggr.R
@@ -0,0 +1,17 @@
+
+serve<-function(callback,port=7777L){
+ stopifnot(is.function(callback));
+ stopifnot(length(port)==1);
+ as.integer(port)->port; 
+ .Call("startTrigger",port,
+   function(x){
+    #DEBUG:
+    if(x=="Q") return(0L);
+    #:DEBUG
+    try(callback(x))->y;
+    if(class(y)=="try-error") return(9L);
+    if(!is.character(y)) return(9L);
+    y<-paste(paste(y,collapse=""),"\r\n\r\n",sep="");
+    return(y);
+   },new.env());
+}
